e7d375c57256fd65f7a80ee1910c0eda5f5cf5c8
W.P. McNeill <wmcneill@us.ibm.com>
2016-08-01 17:28:40 -0500

    Test for input and context batches

--- tests/test_tfrnnlm.py
+++ tests/test_tfrnnlm.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 import numpy as np
-from tfrnnlm.text import IndexedVocabulary, whitespace_word_tokenization, language_model_batches, batches
+from tfrnnlm.text import IndexedVocabulary, whitespace_word_tokenization, language_model_batches_old, language_model_batches
 
 
 class TestTokenization(TestCase):
@@ -31,7 +31,7 @@ class TestIndexing(TestCase):
 
 class TestBatching(TestCase):
     def test_language_model_batches(self):
-        batched_input, batched_targets = language_model_batches([79, 90, 136, 33, 5, 137], 3)
+        batched_input, batched_targets = language_model_batches_old([79, 90, 136, 33, 5, 137], 3)
         np.testing.assert_equal(batched_input, np.array([
             [79, 90, 136],
             [90, 136, 33],
@@ -52,8 +52,36 @@ class TestBatching(TestCase):
 
 class TestBatches(TestCase):
     def test_batches(self):
-        data = range(10)
-        for contexts, targets in batches(data, 3, 4):
-            print(contexts)
-            print(targets)
-            print("\n")
+        lm_batches = list(language_model_batches([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 4))
+        self.assertEquals(len(lm_batches), 3)
+        # Batch 0
+        np.testing.assert_equal(lm_batches[0][0], np.array([[0, 1, 2],
+                                                            [1, 2, 3],
+                                                            [2, 3, 4],
+                                                            [3, 4, 5]]
+                                                           ))
+        np.testing.assert_equal(lm_batches[0][1], np.array([[1, 2, 3],
+                                                            [2, 3, 4],
+                                                            [3, 4, 5],
+                                                            [4, 5, 6]]
+                                                           ))
+        # Batch 1
+        np.testing.assert_equal(lm_batches[1][0], np.array([[4, 5, 6],
+                                                            [5, 6, 7],
+                                                            [6, 7, 8],
+                                                            [7, 8, 9]]
+                                                           ))
+        np.testing.assert_equal(lm_batches[1][1], np.array([[5, 6, 7],
+                                                            [6, 7, 8],
+                                                            [7, 8, 9],
+                                                            [8, 9, 0]]))
+        # Batch 2
+        np.testing.assert_equal(lm_batches[2][0], np.array([[8, 9, 0],
+                                                            [9, 0, 0],
+                                                            [0, 0, 0],
+                                                            [0, 0, 0]]
+                                                           ))
+        np.testing.assert_equal(lm_batches[2][1], np.array([[9, 0, 0],
+                                                            [0, 0, 0],
+                                                            [0, 0, 0],
+                                                            [0, 0, 0]]))
--- tfrnnlm/train.py
+++ tfrnnlm/train.py
@@ -4,7 +4,7 @@ import pickle
 import tensorflow as tf
 from tfrnnlm import logger
 from tfrnnlm.rnn import RNN
-from tfrnnlm.text import whitespace_word_tokenization, IndexedVocabulary, language_model_batches
+from tfrnnlm.text import whitespace_word_tokenization, IndexedVocabulary, language_model_batches_old
 
 
 def train_model(args):
@@ -14,7 +14,7 @@ def train_model(args):
     with open(os.path.join(args.model, "vocabulary"), "wb") as vocabulary_file:
         pickle.dump(vocabulary, vocabulary_file)
     data = [vocabulary.index(token) for token in tokens]
-    batched_input, batched_targets = language_model_batches(data, args.batch_size)
+    batched_input, batched_targets = language_model_batches_old(data, args.batch_size)
     with tf.Graph().as_default():
         model = RNN(len(tokens), args.batch_size, len(vocabulary),
                     args.hidden_units, args.init, args.keep, args.layers,
